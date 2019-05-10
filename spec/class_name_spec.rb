51bfafdcae94d17481e3c7f9f8b006f8baba0156
Myk Klemme <mykklemme@gmail.com>
2019-05-09 22:57:08 -0700

    remove base module test

--- spec/class_name_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ClassName do
-  it "has a version number" do
-    expect(ClassName::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
