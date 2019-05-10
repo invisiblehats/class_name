77e9f97f0d39c03c58a3d9626e9fe5179752c934
Myk Klemme <mykklemme@gmail.com>
2019-05-09 22:56:54 -0700

    Added version test and code

--- lib/class_name/version.rb
+++ lib/class_name/version.rb
@@ -1,3 +1,3 @@
 module ClassName
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'
 end
---
+++ spec/class_name/version_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe ClassName::VERSION do
+  it "has a version number" do
+    expect(ClassName::VERSION).not_to be nil
+  end
+end
